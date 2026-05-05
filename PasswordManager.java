3233a2168e85f209cc6156f4cfb200cd57d1ead0
VANSHIKA <vanshikarajput214@gmail.com>
2026-05-05 18:09:47 +0530

    removed main class code

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class PasswordManager{
-     ArrayList<Credential> list =new ArrayList<>();
+    ArrayList<Credential> list =new ArrayList<>();
     public void addPassword(String website,String username,String password)
-    {   String encrypasswrd= AESUtil.encrypt(password);
+    {  String encrypasswrd= AESUtil.encrypt(password);
         Credential c=new Credential();
         c.website=website;
         c.username=username;
@@ -17,7 +17,15 @@ public class PasswordManager{
     public String getDecryptedPassword(String encrypted) {
        return AESUtil.decrypt(encrypted);
 }
-
-
-   
+    public void deletePassword(String website)
+    {
+      for(int i=0;i<list.size();i++)
+      { Credential c =list.get(i);
+         if (c.website.equals(website))
+         {
+            list.remove(i);
+            break;
+         }
+      }
+    }
 }
