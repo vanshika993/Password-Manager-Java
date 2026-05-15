92bde5800e160832f8d1c67341262f5690d3cd0a
VANSHIKA <vanshikarajput214@gmail.com>
2026-05-15 19:29:07 +0530

    Commit all changes

--- AESUtil.java
+++ AESUtil.java
@@ -1,22 +1,40 @@
-public class AESUtil{
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
-    public static String encrypt(String password)
-    {  String reversed="";
-        for(int i=password.length()-1; i>=0;i--)
-        {
-            reversed+=password.charAt(i);
-             
+public class AESUtil {
+     
+    private static final String SECRET_KEY = "MySecretMasterKey"; 
+
+    
+    public static String encrypt(String password) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes("UTF-8"), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            
+            byte[] encryptedBytes = cipher.doFinal(password.getBytes("UTF-8"));
+         
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return password; 
         }
-        return reversed; 
     }
-    public static String decrypt(String encrypt)
-    {
-        String original="";
-        for(int i=encrypt.length()-1;i>=0;i--)
-        {
-            original+=encrypt.charAt(i);
+
+    
+    public static String decrypt(String encrypted) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes("UTF-8"), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            
+            byte[] decodedBytes = Base64.getDecoder().decode(encrypted);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return encrypted;  
         }
-        return original;
     }
-   
 }
